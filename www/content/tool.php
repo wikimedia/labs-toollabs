wikimedia/labs-toollabs

commit a7c75870e1c33928e051439b7341ab6e1215b475

Reduce font-size for web interface link on tool.php
Change-Id: Ibbdc79b731b9a5e4851468ca18db12596aca2584

www/style.css
@@ -192,6 +192,9 @@ span.subtool-desc {
 .tool-name {
   font-size: 150%;
 }
+.tool-name a {
+  font-size: 67%; /* undo giant font */
+}
 .tool-name span.mw-editsection {
   font-size: 50%;
 }

commit b0dde9407a521fd80b6d32a431efb42b6fa274b0

Fix font-size on tool.php and list.php properly
The fix applied in a7c7587 broke the default tool list page because
I didn't spend enough time to understand the existing styles. Revert the
special sizing for anchor tags inside .tool-name sections and instead
wrap the "web interface" link in a span.wm-editsection tag to match the
markup used in list.php.

Change-Id: Iff2343eb24f796ea19e7c4eb158042588f57e4e5

www/content/tool.php
@@ -17,7 +17,7 @@ if ( $tool !== '' ) {
 <?php
 	echo htmlspecialchars( $tool );
 	if ( array_key_exists( 0, glob( "{$home}/public_html/index.*" ) ) ) {
-		echo '<br/><a href="/', urlencode( $tool ), '/">(Web interface)</a>';
+		echo '<br/><span class="mw-editsection"><a href="/', urlencode( $tool ), '/">(Web interface)</a></span>';
 	}
 ?>
 </th><td></td></tr>

www/style.css
@@ -192,9 +192,6 @@ span.subtool-desc {
 .tool-name {
   font-size: 150%;
 }
-.tool-name a {
-  font-size: 67%; /* undo giant font */
-}
 .tool-name span.mw-editsection {
   font-size: 50%;
 }
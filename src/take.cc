9123056ee76e58c1ac52aa745ed88a21462f6fdd
Tim Landscheidt <tim@tim-landscheidt.de>
2016-02-21 03:00:15 +0000

    Untabify take source
    Change-Id: I3c6b8e6ed9063ad4c7e8fa160573c55c52d96fd3

--- src/take.cc
+++ src/take.cc
@@ -24,158 +24,158 @@
 
 bool refuse(const char* file, const char* why)
 {
-	fprintf(stderr, "%s: %s\n", file, why);
-	return false;
+    fprintf(stderr, "%s: %s\n", file, why);
+    return false;
 }
 
 bool error(const char* file)
 {
-	return refuse(file, strerror(errno));
+    return refuse(file, strerror(errno));
 }
 
 // This is just convenience for autoclosing files and prevent fd leaks
 struct FD {
-	int		fd;
+    int     fd;
 
-			FD(int f): fd(f)		{ };
-			~FD()				{ if(fd >= 0) close(fd); };
-			operator int (void) const	{ return fd; };
+            FD(int f): fd(f)        { };
+            ~FD()               { if(fd >= 0) close(fd); };
+            operator int (void) const   { return fd; };
 };
 
-gid_t	groups[256];
-int	ngroups;
+gid_t   groups[256];
+int ngroups;
 bool cwdchanged = false;
 
 bool takeover(const char* path, bool trustpath)
 {
-	// We open the files and use the file descriptors exclusively
-	// to avoid trickery and race conditions
-	FD file = open(path, O_RDONLY|O_NOFOLLOW);
+    // We open the files and use the file descriptors exclusively
+    // to avoid trickery and race conditions
+    FD file = open(path, O_RDONLY|O_NOFOLLOW);
 
-	if(file < 0) {
-		if(errno == ELOOP)
-			return refuse(path, "will not follow or touch symlinks");
-		return error(path);
-	}
+    if(file < 0) {
+        if(errno == ELOOP)
+            return refuse(path, "will not follow or touch symlinks");
+        return error(path);
+    }
 
-	struct stat	sfile;
+    struct stat sfile;
 
-	if(fstat(file, &sfile))
-		return error(path);
+    if(fstat(file, &sfile))
+        return error(path);
 
-	if(!trustpath) {
-		char	dirname[strlen(path)+1];
+    if(!trustpath) {
+        char    dirname[strlen(path)+1];
 
-		strcpy(dirname, path);
+        strcpy(dirname, path);
 
-		if(path[0] && path[strlen(path)-1]=='/')
-			dirname[strlen(path)-1] = 0;
+        if(path[0] && path[strlen(path)-1]=='/')
+            dirname[strlen(path)-1] = 0;
 
-		if(char* slash = strrchr(dirname, '/'))
-			*slash = 0;
-		else
-			strcpy(dirname, ".");
+        if(char* slash = strrchr(dirname, '/'))
+            *slash = 0;
+        else
+            strcpy(dirname, ".");
 
-		int dir  = open(dirname, O_RDONLY|O_NOFOLLOW);
+        int dir  = open(dirname, O_RDONLY|O_NOFOLLOW);
 
-		if(dir < 0) {
-			if(errno == ELOOP)
-				return refuse(dirname, "will not follow or touch symlinks");
-			return error(dirname);
-		}
+        if(dir < 0) {
+            if(errno == ELOOP)
+                return refuse(dirname, "will not follow or touch symlinks");
+            return error(dirname);
+        }
 
-		struct stat	sdir;
-		if(fstat(dir, &sdir)) {
-			close(dir);
-			return error(dirname);
-		}
+        struct stat sdir;
+        if(fstat(dir, &sdir)) {
+            close(dir);
+            return error(dirname);
+        }
 
-		if(!S_ISDIR(sdir.st_mode)) {
-			close(dir);
-			return refuse(dirname, "containing directory doesn't appear to be... a directory?");
-		}
+        if(!S_ISDIR(sdir.st_mode)) {
+            close(dir);
+            return refuse(dirname, "containing directory doesn't appear to be... a directory?");
+        }
 
-		if(sdir.st_uid != getuid()) {
-			close(dir);
-			return refuse(path, "you must own the containing directory");
-		}
+        if(sdir.st_uid != getuid()) {
+            close(dir);
+            return refuse(path, "you must own the containing directory");
+        }
 
-		// Here, we are being extremely paranoid, and check that the file is actually in the directory.
-		if(sdir.st_dev != sfile.st_dev) {
-			close(dir);
-			return refuse(path, "the file must be on the same filesystem as its directory");
-		}
+        // Here, we are being extremely paranoid, and check that the file is actually in the directory.
+        if(sdir.st_dev != sfile.st_dev) {
+            close(dir);
+            return refuse(path, "the file must be on the same filesystem as its directory");
+        }
 
-		bool found = false;
-		if(DIR* df = fdopendir(dir)) {
-			while(dirent* d = readdir(df)) {
-				if(sfile.st_ino == d->d_ino) {
-					found = true;
-					break;
-				}
-			}
-			closedir(df);
-		} else
-			return error(dirname);
+        bool found = false;
+        if(DIR* df = fdopendir(dir)) {
+            while(dirent* d = readdir(df)) {
+                if(sfile.st_ino == d->d_ino) {
+                    found = true;
+                    break;
+                }
+            }
+            closedir(df);
+        } else
+            return error(dirname);
 
-		if(!found)
-			return refuse(path, "the file isn't in its directory?");
-	}
+        if(!found)
+            return refuse(path, "the file isn't in its directory?");
+    }
 
-	if(!S_ISDIR(sfile.st_mode) && sfile.st_nlink != 1)
-		return refuse(path, "will only touch files with no hardlinks");
+    if(!S_ISDIR(sfile.st_mode) && sfile.st_nlink != 1)
+        return refuse(path, "will only touch files with no hardlinks");
 
-	// check that the group matches
-	bool found = false;
-	for(int i=0; i<ngroups; i++)
-		if(sfile.st_gid == groups[i]) {
-			found = true;
-			break;
-		}
-	if(!found)
-		return refuse(path, "You need to share a group with the file");
+    // check that the group matches
+    bool found = false;
+    for(int i=0; i<ngroups; i++)
+        if(sfile.st_gid == groups[i]) {
+            found = true;
+            break;
+        }
+    if(!found)
+        return refuse(path, "You need to share a group with the file");
 
-	// everything checks out; do it.  Do it!  Doooo it!
-	if(fchown(file, getuid(), -1))
-		return error(path);
+    // everything checks out; do it.  Do it!  Doooo it!
+    if(fchown(file, getuid(), -1))
+        return error(path);
 
-	// recursion for fun and profit
-	if(S_ISDIR(sfile.st_mode)) {
-		if(DIR* df = fdopendir(file)) {
-			bool	ok = true;
+    // recursion for fun and profit
+    if(S_ISDIR(sfile.st_mode)) {
+        if(DIR* df = fdopendir(file)) {
+            bool    ok = true;
 
-			while(dirent* d = readdir(df)) {
-				if(d->d_name[0]=='.') {
-					if(d->d_name[1]==0)
-						continue;
-					if(d->d_name[1]=='.' && d->d_name[2]==0)
-						continue;
-				}
-				fchdir(file);
-				cwdchanged = true;
-				ok &= takeover(d->d_name, true);
-			}
-			closedir(df);
-		} else
-			return error(path);
-	}
+            while(dirent* d = readdir(df)) {
+                if(d->d_name[0]=='.') {
+                    if(d->d_name[1]==0)
+                        continue;
+                    if(d->d_name[1]=='.' && d->d_name[2]==0)
+                        continue;
+                }
+                fchdir(file);
+                cwdchanged = true;
+                ok &= takeover(d->d_name, true);
+            }
+            closedir(df);
+        } else
+            return error(path);
+    }
 
-	return true;
+    return true;
 }
 
 
 int main(int argc, char** argv)
 {
-	ngroups = getgroups(sizeof(groups)/sizeof(groups[0]), groups);
+    ngroups = getgroups(sizeof(groups)/sizeof(groups[0]), groups);
 
-	int cwd = open(".", O_RDONLY);
-	bool ok = true;
-	for(int arg=1; arg<argc; arg++) {
-		if (cwdchanged)
-			fchdir(cwd);
-		ok &= takeover(argv[arg], false);
+    int cwd = open(".", O_RDONLY);
+    bool ok = true;
+    for(int arg=1; arg<argc; arg++) {
+        if (cwdchanged)
+            fchdir(cwd);
+        ok &= takeover(argv[arg], false);
     }
-	close(cwd);
-	return ok? 0: 1;
+    close(cwd);
+    return ok? 0: 1;
 }
 

715be778a37df279f92eba025390964c2c96f59e
Bryan Davis <bd808@wikimedia.org>
2016-04-26 12:16:31 -0600

    Rewrite jsub in python
    Port the jsub job queue wrapper script to python.
    
    The port makes a few small changes to the output that is sent to qsub:
    * `-j yes|no` flag always added
    * Redundant `-e` and `-o` flags stripped
    * `-e` and `-o` flags always added after ~/.jsubrc and cli arguments
    * `-l h_vmem` only applied if it raises the default memory level
    * `-l resource=value,...` exploded into multiple `-l resource=value`
      settings
    * `-b yes` always added
    
    Bug: T132475
    Signed-off-by: Antoine Musso <hashar@free.fr>
    Change-Id: I3565447d02ff718edbdf208c84dfa46bd47130be

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import setuptools
+
+setuptools.setup(
+    name='toollabs-jobutils',
+    version='0.1',
+    packages=None,
+    scripts=[
+        'jobutils/bin/jsub',
+    ],
+    description='Utilities for use on Wikimedia Tool Labs',
+)

5baa25904dee53adf17829c5c79394aa9ada0c46
Bryan Davis <bd808@wikimedia.org>
2020-04-19 18:13:46 -0600

    Format with Black
    * Update tox.ini to add black linter
    * Add flake8 checks for import order and logger usage
    * Reformat files with Black
    * Fix flake8 warnings from new checks
    
    Change-Id: Ifa332e5da87ad5d4c3d71b54e6cc997882c5e1c7

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 import setuptools
 
 setuptools.setup(
-    name='toollabs-jobutils',
-    version='0.1',
+    name="toollabs-jobutils",
+    version="0.1",
     packages=None,
     scripts=[
-        'jobutils/bin/jsub',
+        "jobutils/bin/jsub",
     ],
-    description='Utilities for use on Wikimedia Toolforge',
+    description="Utilities for use on Wikimedia Toolforge",
 )
